3390c327f8f1a0b094aaaf8b8bf2cc037248bc72
amrsabra <amr2905sabra@gmail.com>
2025-06-17 16:57:39 +0100

    hero section/homepage fix

--- app/page.js
+++ app/page.js
@@ -1,14 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Homepage from "@/components/Homepage";
-
-import "@/styles/homepage.scss";
-
+'use client';
+import Homepage from '../components/Homepage';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <Homepage />
-    </div>
-  );
-}
+  return <Homepage />;
+} 
