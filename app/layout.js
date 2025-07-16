0651866e8797ba2626057c5a858c68d5d0eefda1
amrsabra <amr2905sabra@gmail.com>
2025-07-16 15:22:18 +0100

    helo

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { useEffect } from 'react';
-import { supabase } from '../supabaseClient';
 import SubscriptionLinker from "./components/SubscriptionLinker";
 
 const inter = Inter({
